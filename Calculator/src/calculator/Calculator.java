b33483e14ec919b552bc21734e2737ad1c00d4cd
armaztoks <viktorija.grinova@gmail.com>
2019-09-01 20:39:52 +0300

    !

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -8,6 +8,7 @@ package calculator;
 import static calculator.MatematiskasDarbibas.cipars;
 import static calculator.MatematiskasDarbibas.kalkulacijas;
 import static calculator.MatematiskasDarbibas.matematiskaDarbiba;
+import java.util.Scanner;
 
 /**
  *
@@ -18,13 +19,22 @@ public class Calculator {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+public static void main(String[] args) {
+    String ans;
+    do {
+    Scanner sc = new Scanner (System.in);
     double skaitlisViens = cipars();
     double skaitlisDivi = cipars();
     char darbiba = matematiskaDarbiba();
-   double rezultats = kalkulacijas(skaitlisViens,skaitlisDivi,darbiba);
-   System.out.println("Rezultats: " + rezultats);
-   
+    double rezultats = kalkulacijas(skaitlisViens,skaitlisDivi,darbiba);
+    System.out.println("Rezultats: " + rezultats);
+    System.out.println();
+    
+    System.out.print("Vai velies turpinat? ja/ne: ");
+    ans = sc.nextLine();
+    System.out.println();
     }
+    while (ans.equalsIgnoreCase("ja"));
+}
     
 }
